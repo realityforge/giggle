fe949ed857bbce657c15b7843d2c339b836c2446
Peter Donald <peter@realityforge.org>
2019-05-29 20:19:13 +1000

    Add tests for schema arg

--- src/test/java/org/realityforge/giggle/MainTest.java
+++ src/test/java/org/realityforge/giggle/MainTest.java
@@ -1,7 +1,8 @@
 package org.realityforge.giggle;
 
-import java.util.logging.LogRecord;
-import java.util.stream.Collectors;
+import gir.io.FileUtil;
+import java.nio.file.Path;
+import java.util.List;
 import javax.annotation.Nonnull;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
@@ -40,6 +41,48 @@ public class MainTest
     inIsolatedDirectory( () -> {
       final String output = processOptions();
       assertOutputContains( output, "Error: No schema files specified." );
+    } );
+  }
+
+  @Test
+  public void processOptions_schemaNoExist()
+    throws Exception
+  {
+    inIsolatedDirectory( () -> {
+      final String output = processOptions( "--schema", "myschema.graphql" );
+      assertOutputContains( output, "Error: Specified graphql schema file does not exist. Specified value: myschema.graphql" );
+    } );
+  }
+
+  @Test
+  public void processOptions_singleSchema()
+    throws Exception
+  {
+    inIsolatedDirectory( () -> {
+      final Environment environment = newEnvironment();
+      FileUtil.write( "schema.graphql", "ContentIgnored" );
+      Main.processOptions( environment, "--schema", "schema.graphql" );
+
+      final Path file = FileUtil.getCurrentDirectory().resolve( "schema.graphql" );
+      assertEquals( environment.getSchemaFiles().get( 0 ), file );
+    } );
+  }
+
+  @Test
+  public void processOptions_multipleSchemas()
+    throws Exception
+  {
+    inIsolatedDirectory( () -> {
+      final Environment environment = newEnvironment();
+      FileUtil.write( "schema1.graphql", "ContentIgnored" );
+      FileUtil.write( "schema2.graphql", "ContentIgnored" );
+      Main.processOptions( environment, "--schema", "schema1.graphql", "--schema", "schema2.graphql" );
+
+      final Path file1 = FileUtil.getCurrentDirectory().resolve( "schema1.graphql" );
+      final Path file2 = FileUtil.getCurrentDirectory().resolve( "schema2.graphql" );
+      final List<Path> schemaFiles = environment.getSchemaFiles();
+      assertEquals( schemaFiles.get( 0 ), file1 );
+      assertEquals( schemaFiles.get( 1 ), file2 );
     } );
   }
 
