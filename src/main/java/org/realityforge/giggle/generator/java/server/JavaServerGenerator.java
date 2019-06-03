0407b69cf69af667bc9f6d60f33ae444a9ec91df
Peter Donald <peter@realityforge.org>
2019-06-03 16:42:21 +1000

    Extend base generator

--- src/main/java/org/realityforge/giggle/generator/java/server/JavaServerGenerator.java
+++ src/main/java/org/realityforge/giggle/generator/java/server/JavaServerGenerator.java
@@ -3,10 +3,11 @@ package org.realityforge.giggle.generator.java.server;
 import javax.annotation.Nonnull;
 import org.realityforge.giggle.generator.Generator;
 import org.realityforge.giggle.generator.GeneratorContext;
+import org.realityforge.giggle.generator.java.AbstractJavaGenerator;
 
 @Generator.MetaData( name = "java-server" )
 public class JavaServerGenerator
-  implements Generator
+  extends AbstractJavaGenerator
 {
   @Override
   public void generate( @Nonnull final GeneratorContext context )
