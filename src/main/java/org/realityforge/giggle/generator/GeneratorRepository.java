cf71c03414a821d5b3a698a2f0167d24b90c70f7
Peter Donald <peter@realityforge.org>
2019-06-03 11:39:54 +1000

    Load generators from service registry

--- src/main/java/org/realityforge/giggle/generator/GeneratorRepository.java
+++ src/main/java/org/realityforge/giggle/generator/GeneratorRepository.java
@@ -1,9 +1,12 @@
 package org.realityforge.giggle.generator;
 
+import java.util.ServiceLoader;
+
 public final class GeneratorRepository
   extends AbstractGeneratorRepository
 {
   public GeneratorRepository()
   {
+    ServiceLoader.load( Generator.class ).forEach( this::registerGenerator );
   }
 }
