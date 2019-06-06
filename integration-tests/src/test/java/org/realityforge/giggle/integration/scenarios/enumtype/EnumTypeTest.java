894e09212106956e2f21306f535cbc556ad6e374
Peter Donald <peter@realityforge.org>
2019-06-06 12:07:32 +1000

    Restructure test

--- integration-tests/src/test/java/org/realityforge/giggle/integration/scenarios/enumtype/EnumTypeTest.java
+++ integration-tests/src/test/java/org/realityforge/giggle/integration/scenarios/enumtype/EnumTypeTest.java
@@ -7,16 +7,27 @@ import org.realityforge.giggle.integration.scenarios.AbstractScenarioTest;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
-@SuppressWarnings( "deprecation" )
 public class EnumTypeTest
   extends AbstractScenarioTest
 {
   @Test
-  public void scenario()
+  public void structure()
+  {
+    assertTrue( Modifier.isPublic( EventType.class.getModifiers() ) );
+  }
+
+  @SuppressWarnings( "deprecation" )
+  @Test
+  public void deprecatedEnumValue()
     throws Exception
   {
     assertNotNull( EventType.class.getField( EventType.RegenBurn.name() ).getAnnotation( Deprecated.class ) );
-    assertTrue( Modifier.isPublic( EventType.class.getModifiers() ) );
+  }
+
+  @Test
+  public void typeMapping()
+    throws Exception
+  {
     final Map<String, String> typeMapping = new HashMap<>();
     typeMapping.put( "EventType", EventType.class.getName() );
     assertTypeMapping( typeMapping );
