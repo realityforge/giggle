3d73b15bf9f5535904603fb4f8477acbfebdcf33
Peter Donald <peter@realityforge.org>
2019-06-12 09:28:13 +1000

    Add stub for client-side generator

---
+++ compiler/src/main/java/org/realityforge/giggle/generator/java/client/JavaClientGenerator.java
@@ -0,0 +1,17 @@
+package org.realityforge.giggle.generator.java.client;
+
+import javax.annotation.Nonnull;
+import org.realityforge.giggle.generator.Generator;
+import org.realityforge.giggle.generator.GeneratorContext;
+import org.realityforge.giggle.generator.java.AbstractJavaGenerator;
+
+@Generator.MetaData( name = "java-client" )
+public class JavaClientGenerator
+  extends AbstractJavaGenerator
+{
+  @Override
+  public void generate( @Nonnull final GeneratorContext context )
+    throws Exception
+  {
+  }
+}
