f2d15e96f8f738a88d72bbf7751f77757b48f5d4
Peter Donald <peter@realityforge.org>
2019-06-12 09:32:40 +1000

    Add scaffolding for generating fragments and operations from documents

--- compiler/src/main/java/org/realityforge/giggle/generator/java/client/JavaClientGenerator.java
+++ compiler/src/main/java/org/realityforge/giggle/generator/java/client/JavaClientGenerator.java
@@ -1,5 +1,9 @@
 package org.realityforge.giggle.generator.java.client;
 
+import graphql.language.Definition;
+import graphql.language.FragmentDefinition;
+import graphql.language.OperationDefinition;
+import java.io.IOException;
 import javax.annotation.Nonnull;
 import org.realityforge.giggle.generator.Generator;
 import org.realityforge.giggle.generator.GeneratorContext;
@@ -12,6 +16,28 @@ public class JavaClientGenerator
   @Override
   public void generate( @Nonnull final GeneratorContext context )
     throws Exception
+  {
+    for ( final Definition definition : context.getDocument().getDefinitions() )
+    {
+      if ( definition instanceof FragmentDefinition )
+      {
+        emitFragment( context, (FragmentDefinition) definition );
+      }
+      else
+      {
+        assert definition instanceof OperationDefinition;
+        emitOperation( context, (OperationDefinition) definition );
+      }
+    }
+  }
+
+  private void emitFragment( @Nonnull final GeneratorContext context, @Nonnull final FragmentDefinition fragment )
+    throws IOException
+  {
+  }
+
+  private void emitOperation( @Nonnull final GeneratorContext context, @Nonnull final OperationDefinition operation )
+    throws IOException
   {
   }
 }
