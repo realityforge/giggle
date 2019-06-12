39ad420ba303f7263316e6ce83eee33c0daa7253
Peter Donald <peter@realityforge.org>
2019-06-12 17:33:09 +1000

    Add fixture that tests fragments

---
+++ compiler/src/test/fixtures/multiple_operations/output/java-client/com/example/multiple_operations/EventType.java
@@ -0,0 +1,10 @@
+package com.example.multiple_operations;
+
+import javax.annotation.Generated;
+
+@Generated("org.realityforge.giggle.Main")
+public enum EventType {
+  Burn,
+
+  Wildfire
+}
---
+++ compiler/src/test/fixtures/multiple_operations/output/java-client/com/example/multiple_operations/OtherEventQueryResponse.java
@@ -0,0 +1,58 @@
+package com.example.multiple_operations;
+
+import javax.annotation.Generated;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+@Generated("org.realityforge.giggle.Main")
+public final class OtherEventQueryResponse {
+  @Nullable
+  private Event event;
+
+  @Nullable
+  public Event getEvent() {
+    return event;
+  }
+
+  public void setEvent(@Nullable final Event event) {
+    this.event = event;
+  }
+
+  public static final class Event {
+    @Nonnull
+    private String id;
+
+    @Nullable
+    private String name;
+
+    @Nullable
+    private EventType type;
+
+    @Nonnull
+    public String getId() {
+      return id;
+    }
+
+    public void setId(@Nonnull final String id) {
+      this.id = id;
+    }
+
+    @Nullable
+    public String getName() {
+      return name;
+    }
+
+    public void setName(@Nullable final String name) {
+      this.name = name;
+    }
+
+    @Nullable
+    public EventType getType() {
+      return type;
+    }
+
+    public void setType(@Nullable final EventType type) {
+      this.type = type;
+    }
+  }
+}
