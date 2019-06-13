199f5f02b9465e5c1286b5ac557f30efba110c98
Peter Donald <peter@realityforge.org>
2019-06-13 16:46:16 +1000

    Remove unused constructors

--- compiler/src/main/java/org/realityforge/giggle/TerminalStateException.java
+++ compiler/src/main/java/org/realityforge/giggle/TerminalStateException.java
@@ -7,19 +7,9 @@ final class TerminalStateException
 {
   private final int _exitCode;
 
-  TerminalStateException( final int exitCode )
-  {
-    this( null, exitCode );
-  }
-
   TerminalStateException( @Nullable final String message, final int exitCode )
   {
-    this( message, null, exitCode );
-  }
-
-  TerminalStateException( @Nullable final String message, @Nullable final Throwable cause, final int exitCode )
-  {
-    super( message, cause );
+    super( message, null );
     _exitCode = exitCode;
   }
 
